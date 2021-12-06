e4c30b87045aab49ff2d3667a94a9e78247f4d89
1e1001 <michael@malinov.com>
2021-12-05 21:04:45 -0800

    add command logging to other scripts
    Signed-off-by: 1e1001 <michael@malinov.com>

--- compile.sh
+++ compile.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-rm -r out
-mkdir -pv out/
-cp README.md tcpmux.cfg tcpmux tcpmux_start tcpmux_stop tcpmux.service out/
-deno bundle --unstable daemon.ts out/tcpmux.compiled.js
-cat out/tcpmux.compiled.js | terser/node_modules/terser/bin/terser --module -m reserved=['_'] -c >>out/tcpmux_start
-rm out/tcpmux.compiled.js
+function run {
+	echo $@
+	$@
+}
+run rm -r out
+run mkdir -pv out/
+run cp README.md tcpmux.cfg tcpmux tcpmux_start tcpmux_stop tcpmux.service out/
+run deno bundle --unstable daemon.ts out/tcpmux.compiled.js
+run cat out/tcpmux.compiled.js | terser/node_modules/terser/bin/terser --module -m reserved=['_'] -c >>out/tcpmux_start
+run rm out/tcpmux.compiled.js
 # cd out
 # tar czvf release.tar.gz tcpmux
 # cd ..
--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
-mkdir -pv /etc/tcpmux/
-cp out/tcpmux /etc/tcpmux/
-cp out/tcpmux_start /etc/tcpmux/
-cp out/tcpmux_stop /etc/tcpmux/
-cp out/tcpmux.service /usr/lib/systemd/
+#!/bin/bash
+function run {
+	echo $@
+	$@
+}
+run mkdir -pv /etc/tcpmux/
+run cp out/tcpmux /etc/tcpmux/
+run cp out/tcpmux_start /etc/tcpmux/
+run cp out/tcpmux_stop /etc/tcpmux/
+run cp out/tcpmux.service /usr/lib/systemd/
 # copy the config if it doesn't exist
 if ! [ -f /etc/tcpmux/tcpmux.cfg ]; then
-	cp out/tcpmux.cfg /usr/lib/systemd/
+	run cp out/tcpmux.cfg /usr/lib/systemd/
 fi
