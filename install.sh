95b4af7ec0a89cd7fd915f6226c5c5b0eea731a5
1e1001 <michael@malinov.com>
2021-12-05 21:17:49 -0800

    install daemon in the correct location
    Signed-off-by: 1e1001 <michael@malinov.com>

--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@ run mkdir -pv /etc/tcpmux/
 run cp out/tcpmux /etc/tcpmux/
 run cp out/tcpmux_start /etc/tcpmux/
 run cp out/tcpmux_stop /etc/tcpmux/
-run cp out/tcpmux.service /usr/lib/systemd/
+run cp out/tcpmux.service /etc/systemd/system/
 # copy the config if it doesn't exist
 if ! [ -f /etc/tcpmux/tcpmux.cfg ]; then
-	run cp out/tcpmux.cfg /usr/lib/systemd/
+	run cp out/tcpmux.cfg /etc/tcpmux/
 fi
 echo ">> # install done"
