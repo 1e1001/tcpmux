9b8927b5452bdd2b697b05822780918bbd8041e5
1e1001 <michael@malinov.com>
2021-12-05 20:55:32 -0800

    don't create a tarball
    Signed-off-by: 1e1001 <michael@malinov.com>

--- compile.sh
+++ compile.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 rm -r out
-mkdir -pv out/tcpmux/
-cp README.md tcpmux.cfg tcpmux tcpmux_start tcpmux_stop tcpmux.service out/tcpmux/
-deno bundle --unstable daemon.ts out/tcpmux/tcpmux.compiled.js
-cat out/tcpmux/tcpmux.compiled.js | terser/node_modules/terser/bin/terser --module -m reserved=['_'] -c >>out/tcpmux/tcpmux_start
-rm out/tcpmux/tcpmux.compiled.js
-cd out
-tar czvf release.tar.gz tcpmux
-cd ..
+mkdir -pv out/
+cp README.md tcpmux.cfg tcpmux tcpmux_start tcpmux_stop tcpmux.service out/
+deno bundle --unstable daemon.ts out/tcpmux.compiled.js
+cat out/tcpmux.compiled.js | terser/node_modules/terser/bin/terser --module -m reserved=['_'] -c >>out/tcpmux_start
+rm out/tcpmux.compiled.js
+# cd out
+# tar czvf release.tar.gz tcpmux
+# cd ..
--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 mkdir -pv /etc/tcpmux/
-cp out/tcpmux/tcpmux /etc/tcpmux/
-cp out/tcpmux/tcpmux_start /etc/tcpmux/
-cp out/tcpmux/tcpmux_stop /etc/tcpmux/
-cp out/tcpmux/tcpmux.service /usr/lib/systemd/
+cp out/tcpmux /etc/tcpmux/
+cp out/tcpmux_start /etc/tcpmux/
+cp out/tcpmux_stop /etc/tcpmux/
+cp out/tcpmux.service /usr/lib/systemd/
 # copy the config if it doesn't exist
 if ! [ -f /etc/tcpmux/tcpmux.cfg ]; then
-	cp out/tcpmux/tcpmux.cfg /usr/lib/systemd/
+	cp out/tcpmux.cfg /usr/lib/systemd/
 fi
