368e94af1922538838ab10204b692e22be1be2e9
1e1001 <michael@malinov.com>
2021-12-05 21:06:14 -0800

    fix compile script logging
    Signed-off-by: 1e1001 <michael@malinov.com>

--- compile.sh
+++ compile.sh
@@ -7,7 +7,8 @@ run rm -r out
 run mkdir -pv out/
 run cp README.md tcpmux.cfg tcpmux tcpmux_start tcpmux_stop tcpmux.service out/
 run deno bundle --unstable daemon.ts out/tcpmux.compiled.js
-run cat out/tcpmux.compiled.js | terser/node_modules/terser/bin/terser --module -m reserved=['_'] -c >>out/tcpmux_start
+echo "cat out/tcpmux.compiled.js | terser/node_modules/terser/bin/terser --module -m reserved=['_'] -c >>out/tcpmux_start"
+cat out/tcpmux.compiled.js | terser/node_modules/terser/bin/terser --module -m reserved=['_'] -c >>out/tcpmux_start
 run rm out/tcpmux.compiled.js
 # cd out
 # tar czvf release.tar.gz tcpmux
