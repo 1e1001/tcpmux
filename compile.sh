df7c11ca8f3fc5dd799cee145c1f4fbc8d22b04e
1e1001 <michael@malinov.com>
2021-12-07 19:27:52 -0800

    fix compile script harder
    Signed-off-by: 1e1001 <michael@malinov.com>

--- compile.sh
+++ compile.sh
@@ -3,8 +3,8 @@ function run {
 	echo ">>" $@
 	$@
 }
-run rm -r out
 run mkdir -pv out/
+run rm out/*
 run cp README.md tcpmux.cfg tcpmux tcpmux_start tcpmux_stop tcpmux.service out/
 run deno bundle --unstable daemon.ts out/tcpmux.compiled.js
 run terser/node_modules/terser/bin/terser --module -m reserved=['_'] out/tcpmux.compiled.js -c -o out/tcpmux.min.js
