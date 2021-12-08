3f29b6cf79a3f4e87d3bcef438a40e3b5900907a
1e1001 <michael@malinov.com>
2021-12-07 19:40:34 -0800

    rename harder
    Signed-off-by: 1e1001 <michael@malinov.com>

--- compile.sh
+++ compile.sh
@@ -5,9 +5,9 @@ function run {
 }
 run mkdir -pv out/
 run rm out/*
-run cp README.md tcpmux* config.txt out/
+run cp README.md tcpmux tcpmux_start tcpmux_stop tcpmux.service config.txt out/
 run deno bundle --unstable daemon.ts out/tcpmux.compiled.js
-run terser/node_modules/terser/bin/terser --module -m reserved=['_'] out/tcpmux.compiled.js -c -o out/tcpmux.min.js
-cat out/tcpmux.min.js >>out/tcpmux_start
-run rm out/tcpmux.compiled.js out/tcpmux.min.js
+run terser/node_modules/terser/bin/terser --module -m reserved=['_'] out/tcpmux.compiled.js -c -o out/tcpmux.js
+cat out/tcpmux.js >>out/tcpmux_start
+run rm out/tcpmux.compiled.js out/tcpmux.js
 echo ">> # compile done"
