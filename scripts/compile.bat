e7228bdbfaa431bffd6137a096803601e6cfaac2
Evyn <baconz@disroot.org>
2021-12-07 20:43:11 -0800

    Update compile.bat
    fUck

--- scripts/compile.bat
+++ scripts/compile.bat
@@ -1,7 +1,10 @@
 @echo off
 rmdir /s /q out
 mkdir out
-copy README.md+src\config.txt+scripts\tcpmux.bat out
+rem windows is stupid and won't copy it on one line, for some reason
+copy README.md out
+copy src\config.txt out
+copy scripts\tcpmux.bat out
 echo deno bundle --unstable src\daemon.ts out\tcpmux.js
 deno bundle --unstable src\daemon.ts out\tcpmux.js
 echo # compile done
